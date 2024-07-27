9cfe7b7af1cbfb35b5e6a6a218a1263f37dc5aae
Nina <159460273+ninalptv@users.noreply.github.com>
2024-07-27 11:27:57 +0300

    Update 7_7.cpp

--- 7_7.cpp
+++ 7_7.cpp
@@ -1,15 +1,15 @@
-//Вернитесь к программе из листинга 7.7 и замените три функции обработки
-//массивов версиями, которые работают с диапазонами значений, заданными парой
-//указателей.Функция f ill_array() вместо возврата действительного
-//количества прочитанных значений должна возвращать указатель на место, следующее за
-//последним введенным элементом; прочие функции должны использовать его в
-//качестве второго аргумента для идентификации конца диапазона данных.
+//Р’РµСЂРЅРёС‚РµСЃСЊ Рє РїСЂРѕРіСЂР°РјРјРµ РёР· Р»РёСЃС‚РёРЅРіР° 7.7 Рё Р·Р°РјРµРЅРёС‚Рµ С‚СЂРё С„СѓРЅРєС†РёРё РѕР±СЂР°Р±РѕС‚РєРё
+//РјР°СЃСЃРёРІРѕРІ РІРµСЂСЃРёСЏРјРё, РєРѕС‚РѕСЂС‹Рµ СЂР°Р±РѕС‚Р°СЋС‚ СЃ РґРёР°РїР°Р·РѕРЅР°РјРё Р·РЅР°С‡РµРЅРёР№, Р·Р°РґР°РЅРЅС‹РјРё РїР°СЂРѕР№
+//СѓРєР°Р·Р°С‚РµР»РµР№.Р¤СѓРЅРєС†РёСЏ f ill_array() РІРјРµСЃС‚Рѕ РІРѕР·РІСЂР°С‚Р° РґРµР№СЃС‚РІРёС‚РµР»СЊРЅРѕРіРѕ
+//РєРѕР»РёС‡РµСЃС‚РІР° РїСЂРѕС‡РёС‚Р°РЅРЅС‹С… Р·РЅР°С‡РµРЅРёР№ РґРѕР»Р¶РЅР° РІРѕР·РІСЂР°С‰Р°С‚СЊ СѓРєР°Р·Р°С‚РµР»СЊ РЅР° РјРµСЃС‚Рѕ, СЃР»РµРґСѓСЋС‰РµРµ Р·Р°
+//РїРѕСЃР»РµРґРЅРёРј РІРІРµРґРµРЅРЅС‹Рј СЌР»РµРјРµРЅС‚РѕРј; РїСЂРѕС‡РёРµ С„СѓРЅРєС†РёРё РґРѕР»Р¶РЅС‹ РёСЃРїРѕР»СЊР·РѕРІР°С‚СЊ РµРіРѕ РІ
+//РєР°С‡РµСЃС‚РІРµ РІС‚РѕСЂРѕРіРѕ Р°СЂРіСѓРјРµРЅС‚Р° РґР»СЏ РёРґРµРЅС‚РёС„РёРєР°С†РёРё РєРѕРЅС†Р° РґРёР°РїР°Р·РѕРЅР° РґР°РЅРЅС‹С….
 #include <iostream>
 using namespace std;
 const int Max = 5;
 
 double * fill_array(double* arBegin, double* arEnd);
-void show_array(double* arBegin, double* arEnd); // не изменять данные
+void show_array(double* arBegin, double* arEnd); 
 void revalue(double r, double* arBegin, double* arEnd);
 int main()
 {
@@ -19,14 +19,14 @@ int main()
 	show_array(properties, arEnd);
 	if (arEnd != properties)
 	{
-		cout << "Enter revaluation factor: "; // ввод коэффициента переоценки
+		cout << "Enter revaluation factor: "; 
 		double factor;
-		while (!(cin >> factor)) // неправильный ввод
+		while (!(cin >> factor)) 
 		{
 			cin.clear();
 			while (cin.get() != '\n')
 				continue;
-			cout << "Bad input; Please enter a number: "; // повторный запрос на ввод числа
+			cout << "Bad input; Please enter a number: "; 
 		}
 		revalue(factor, properties, arEnd);
 		show_array(properties, arEnd);
@@ -52,16 +52,16 @@ double* fill_array(double* arBegin, double* arEnd)
 			cin.clear();
 			while (cin.get() != '\n')
 				continue;
-			cout << "Bad input; input process terminated.\n"; // процесс ввода прекращен
+			cout << "Bad input; input process terminated.\n"; 
 			break;
 		}
-		else if (temp < 0) // сигнал завершения
+		else if (temp < 0) 
 			break;
 		*ptr = temp;
 	}
 	return ptr;
 }
-// Следующая функция может использовать, но не изменять, массив по адресу аг
+
 void show_array(double* arBegin, double* arEnd)
 {
 	int i = 0;
@@ -71,7 +71,7 @@ void show_array(double* arBegin, double* arEnd)
 		cout << *ptr << endl;
 	}
 }
-// Умножает на г каждый элемент аг[]
+
 void revalue(double r, double* arBegin, double* arEnd)
 {
 	double* pt;
